02ff78a7a47a651a86429a1de579b061b3d3390a
pgobin <pgobin@uw.edu>
2025-01-09 18:57:49 -0800

    implemented basic buy system

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Card from "../ui/components/Card";
 import prizes_data from "./mockprizes.json";
+import user_data from "../participant/mockuser.json";
 
-export default function page() {
+export default function Page() {
     // View Prize Availability (Simple card component), displays:
     //  - Prize name
     //  - Prize description
@@ -13,6 +16,12 @@ export default function page() {
     // Incorporate buy component into prize Card component (include buy quantity input for eligible prizes)
 
     const prizes = prizes_data["prizes"];
+    const { hackeroons } = user_data["user"];
+    const [hackeroonAmount, setHackeroonAmount] = useState(hackeroons);
+
+    useEffect(() => {
+        console.log(hackeroonAmount);
+    }, [hackeroonAmount]);
 
     return (
         <div className="">
@@ -22,7 +31,12 @@ export default function page() {
                 {/* Prize Card Components Container */}
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 px-8 pb-4 gap-6">
                     {prizes.map((prize, index) => (
-                        <Card key={index} prize={prize} />
+                        <Card
+                            key={index}
+                            prize={prize}
+                            hackeroonAmount={hackeroonAmount}
+                            setHackeroonAmount={setHackeroonAmount}
+                        />
                     ))}
                 </div>
             </section>
