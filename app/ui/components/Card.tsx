1e6ceb0655d5509556946f55fbc81a0a83d98a64
pgobin <pgobin@uw.edu>
2025-01-12 14:36:24 -0800

    added some buying item logic (deduct from user total hackeroons) and reduce stock. Implemented shopping cart(-ish) functionality

--- app/ui/components/Card.tsx
+++ app/ui/components/Card.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 
 type Prize = {
+    id: number;
     name: string;
     description: string;
     stock: number;
@@ -13,21 +14,44 @@ type CardProps = {
     prize: Prize;
     hackeroonAmount: number;
     setHackeroonAmount: React.Dispatch<React.SetStateAction<number>>;
+    setSelectedItems: React.Dispatch<React.SetStateAction<Prize[]>>;
 };
 
 export default function Card({
     prize,
     hackeroonAmount,
     setHackeroonAmount,
+    setSelectedItems,
 }: CardProps) {
+    const [isItemBought, setIsItemBought] = useState(false);
+    const [prizeStock, setPrizeStock] = useState(prize.stock);
     const buyItem = (itemPrice: number) => {
-        if (hackeroonAmount - itemPrice >= 0) {
+        // If item is not bought, user has enough hackeroons to buy item, and item stock is > 0
+        if (
+            isItemBought === false &&
+            hackeroonAmount - itemPrice >= 0 &&
+            prizeStock > 0
+        ) {
+            // Buy item
             setHackeroonAmount((prev) => prev - itemPrice);
-            setItemBought(true);
+            setIsItemBought(true);
+            // Decrement Stock
+            setPrizeStock((prev) => prev - 1);
+            // Add item to selected items
+            setSelectedItems((prev) => [...prev, prize]);
+        }
+        // If item is bought, unbuy item and refund hackeroons
+        if (isItemBought === true) {
+            setHackeroonAmount((prev) => prev + itemPrice);
+            setIsItemBought(false);
+            // Increment Stock
+            setPrizeStock((prev) => prev + 1);
+            // Remove item from selected items
+            setSelectedItems((prev) =>
+                prev.filter((item) => item.id !== prize.id),
+            );
         }
     };
-
-    const [itemBought, setItemBought] = useState(false);
 
     return (
         <div className="flex flex-col justify-center p-8 min-h-[200px] bg-neutral-200 rounded-lg border-2 border-neutral-400 shadow-md">
@@ -45,18 +69,18 @@ export default function Card({
             <h2 className="text-center text-2xl">{prize.name}</h2>
             {/* Stock and Price */}
             <div className="flex justify-around py-1">
-                <p>{`${prize.stock} Left!`}</p>
+                <p>{`${prizeStock} Left!`}</p>
                 <p>{`${prize.price} Hackeroons`}</p>
             </div>
             {/* Description */}
             <h3 className="text-center">{prize.description}</h3>
             <button
-                className={`py-4 mt-4 rounded-md text-white ${itemBought ? "bg-green-900" : "bg-green-500"}`}
+                className={`py-4 mt-4 rounded-md text-white border-[1px] border-black ${isItemBought ? "bg-red-500" : "bg-green-500"}`}
                 onClick={() => {
                     buyItem(prize.price);
                 }}
             >
-                {itemBought ? "Bought" : "Buy"}
+                {isItemBought ? "Remove" : "Buy"}
             </button>
         </div>
     );
