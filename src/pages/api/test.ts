2bed1686caa2d8df639934898248ff25c453cdc1
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-01-12 16:34:07 -0800

    minor changes

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -4,7 +4,11 @@ import sql from '@/src/util/database';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {  
+
       const data = await sql`SELECT * FROM test`;
+      
+      
+
       res.status(200).json({data});
     } catch(error) {
       res.status(400).json(error);
