355fb0532fcec5a9d3d6fb9026f468663997c617
sam-dp <cloudysaga@gmail.com>
2025-01-18 14:20:09 -0800

    Implement test route for events, improve comments for test_user_transactions

--- src/pages/api/test_user_transactions.ts
+++ src/pages/api/test_user_transactions.ts
@@ -1,20 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import sql from '@/src/util/database';
 
-// src/pages/api/test_transactions.ts
-//
+// src/pages/api/test_user_transactions.ts
+// 
+// Summary:
 // Retrieves all 'transactions' rows for a given 'user' id
 //
+// Parameters:
+// (Required) user: int - user id to return transactions for
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
   if (req.method === 'GET') {
     const { user } = req.query;
 
-    // Missing 'user' id
+    // user is required parameter
     if (!user || typeof user !== 'string') {
       return res.status(400).json({ error: 'Missing or invalid "user" parameter' });
     }
 
-    // Query for transactions
     try {
       const data = await sql`SELECT * FROM transactions WHERE "user" = ${user}`;
       res.status(200).json({ data });
@@ -23,7 +27,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(500).json({ error: 'Internal server error' });
     }
   
-  // Incorrect method
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).json({ error: 'Method not allowed' });
