b762296a7131d74b532c142944484d829a5f8f8b
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-01-17 16:34:30 -0800

    added database integration to leaderboard page

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,20 +1,42 @@
-import React from "react";
-import users_data from "./mockusers.json";
+"use client"
+
+import React, { useEffect, useState } from "react";
+
+/*
+TODO CUSTOM TYPES?
+idk if custom types that are the result of complex queries should be listed here or dataTypes
+imma keep it here for now
+-Simon
+*/
+type Leader = {
+    name: string;
+    balance: number;
+}
 
 export default function Page() {
-    // sort users in descending order by their hackeroon count
-    const users = users_data["users"].sort(
-        (a, b) => b.hackeroons - a.hackeroons,
-    );
+
+    const [leaders, setLeaders] = useState<Leader[]>([]);  
+
+    useEffect(() => {
+
+        fetch("api/leaderboard")
+        .then((response) => response.json())
+        .then((remote_data) => {
+
+            setLeaders(remote_data as Leader[]);
+            console.log(remote_data);
+        })
+        .catch((error) => console.error("Failed to load leaderboard:",error));
+    }, [])
 
     return (
         <div className="w-full grid place-content-center">
             <div className="m-12 w-[60vw] grid place-content-center">
                 <h1 className="text-center text-5xl">Leaderboard</h1>
                 <ol className="mt-6 space-y-6 text-2xl list-decimal">
-                    {users.map((user) => (
-                        <li key={user.id} className="w-fit">
-                            {user.name} --- {user.hackeroons}
+                    {leaders.map((leader, i) => (
+                        <li key={i} className="w-fit">
+                            {leader.name} --- {leader.balance}
                         </li>
                     ))}
                 </ol>
