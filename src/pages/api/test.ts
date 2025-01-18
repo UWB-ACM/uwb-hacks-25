a199370b1178abc2787c2ac6362e86b21b458c5c
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-01-17 16:34:30 -0800

    added database integration to leaderboard page

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -2,16 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import sql from '@/src/util/database';
  
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    try {  
+  if (req.method !== 'GET') return;
 
-      const data = await sql`SELECT * FROM test`;
-      
-      
+  try {  
 
-      res.status(200).json({data});
-    } catch(error) {
-      res.status(400).json(error);
-    }
+    const data = await sql`SELECT * FROM test`;
+
+    res.status(200).json({data});
+  } catch(error) {
+    res.status(400).json(error);
   }
 }
