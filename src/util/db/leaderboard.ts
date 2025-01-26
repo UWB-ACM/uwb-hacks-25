ceee3afda7411be85eab548245682fdb5e683e3f
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-01-25 18:23:50 -0800

    added a new type for leaderboard record

--- src/util/db/leaderboard.ts
+++ src/util/db/leaderboard.ts
@@ -1,20 +1,17 @@
-import { User } from "@/src/util/dataTypes";
+import { LeaderboardRecord } from "@/src/util/dataTypes";
 import sql from "@/src/util/database";
 
 /**
  * Gets the top 5 largest earners.
  */
-export async function getLeaderboard(): Promise<User[]> {
-    const data = await sql`SELECT name, balance from balances 
+export async function getLeaderboard(): Promise<LeaderboardRecord[]> {
+    const data = await sql`SELECT user, name, balance from balances 
          JOIN users ON users.id = balances.user
          ORDER BY balance DESC LIMIT 5`;
 
     return data.map((row) => ({
-        id: row.id,
-        googleId: row.googleId,
+        id: row.user,
         name: row.name,
-        email: row.email,
-        picture: row.picture,
         balance: row.balance
     }));
 }
