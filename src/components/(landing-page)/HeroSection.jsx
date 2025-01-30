4647a3ab15390d10f9aef73c18e4c6f2515b1c1b
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-01-30 14:45:23 -0800

    hero section

---
+++ src/components/(landing-page)/HeroSection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import HackathonDates from "@/src/components/(landing-page)/HackathonDates";
+import VolunteerSection from "@/src/components/(landing-page)/VolunteerSection";
+import LogoContainer from "@/src/components/(landing-page)/LogoContainer";
+import Countdown from "@/src/components/(landing-page)/Countdown";
+
+const HeroSection = () => {
+    return (
+        <div className="justify-center grid sm:grid-cols-3 sm:gap-x-[5rem] sm:flex">
+            <LogoContainer/>
+            <Countdown/>
+
+            <div className="grid grid-cols-subgrid gap-1">
+                {/* Dates of Hackathon + Volunteer Apply button section in here */}
+                <HackathonDates />
+                <VolunteerSection />
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
