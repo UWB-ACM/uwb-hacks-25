baf4270b9c8b7263a2e9da76225a940b25762deb
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-01-30 14:45:23 -0800

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import TestUserName from "@/src/components/TestUserName";
 import TestRecords from "@/src/components/TestRecords";
+import HeroSection from "@/src/components/(landing-page)/HeroSection";
 
 export default function Home() {
     return (
-        <div className="w-full h-screen flex flex-col justify-center items-center">
-            <TestUserName />
-
-            <TestRecords />
+        <div className="max-w-[80vw] h-screen flex flex-col justify-center items-center mx-auto">
+            {/* <TestUserName />
+            <TestRecords /> */}
+            <HeroSection/>
+            
         </div>
     );
 }
---
+++ src/components/(landing-page)/VolunteerSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VolunteerSection = () => {
+    return (
+        <div className="border-[0.25rem] rounded-sm border-black p-[2rem] mb-[2rem]">
+            {"Be the power behind the superpowers! Volunteer now! "}
+            {"<VolunteerButton/>"}
+        </div>
+    );
+};
+
+export default VolunteerSection;
