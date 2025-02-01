81039f514bfc1c49a91f982b5e1aebcf152400b1
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-02-01 15:36:41 -0800

    about section

--- src/components/(landing-page)/AboutSection.jsx
+++ src/components/(landing-page)/AboutSection.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import HackathonDates from "@/src/components/(landing-page)/(HeroSectionComponents)/HackathonDates";
+import VolunteerSection from "@/src/components/(landing-page)/(HeroSectionComponents)/VolunteerSection";
+import LogoContainer from "@/src/components/(landing-page)/(HeroSectionComponents)/LogoContainer";
+import Countdown from "@/src/components/(landing-page)/(HeroSectionComponents)/Countdown";
 const AboutSection = () => {
-  return (
-    <div>AboutSection</div>
-  )
-}
+    return (
+        <div className="justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
+            <LogoContainer />
 
-export default AboutSection
+            <div className="grid grid-cols-subgrid gap-1">
+                {/* Dates of Hackathon + Volunteer Apply button section in here */}
+                <HackathonDates />
+                <VolunteerSection />
+            </div>
+        </div>
+    );
+};
+
+export default AboutSection;
