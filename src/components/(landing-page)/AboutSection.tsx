88e5a674a377fd2f65f44f62c0aa8c612ac650d2
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-02-03 08:58:50 -0800

    boxes

--- src/components/(landing-page)/AboutSection.tsx
+++ src/components/(landing-page)/AboutSection.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import HackathonDates from "@/src/components/(landing-page)/(HeroSectionComponents)/HackathonDates";
-import VolunteerSection from "@/src/components/(landing-page)/(HeroSectionComponents)/VolunteerSection";
-import LogoContainer from "@/src/components/(landing-page)/(HeroSectionComponents)/LogoContainer";
-import Countdown from "@/src/components/(landing-page)/(HeroSectionComponents)/Countdown";
 const AboutSection = () => {
     return (
-        <div className="justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
-            <LogoContainer />
-
-            <div className="grid grid-cols-subgrid gap-1">
-                {/* Dates of Hackathon + Volunteer Apply button section in here */}
-                <HackathonDates />
-                <VolunteerSection />
-            </div>
+        <div className="border-black border-[0.25rem] p-[10rem] mb-[1rem] justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
+            About Section
         </div>
     );
 };
