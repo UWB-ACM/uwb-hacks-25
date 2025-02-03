2c17db6264e6730bdc2143a29ffca871e8127db5
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-02-03 08:58:50 -0800

    boxes

---
+++ src/components/(landing-page)/ScheduleSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ScheduleSection = () => {
+  return (
+      <div className="border-black border-[0.25rem] p-[10rem] mb-[1rem] mt-[1rem] justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
+          Schedule Section
+      </div>
+  );
+}
+
+export default ScheduleSection
