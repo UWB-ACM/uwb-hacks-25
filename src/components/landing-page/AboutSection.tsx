ac07553d6a5dd2ff4f8a9026213b00a3a9bd5b5f
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-02-06 13:30:21 -0800

    basic layout complete for desktop

--- src/components/landing-page/AboutSection.tsx
+++ src/components/landing-page/AboutSection.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 const AboutSection = () => {
     return (
-        <div className="bg-blue-300 grow"
-        >
-            {/* About Section */}
+        <div className="grow border-black border-[0.25rem] p-[10rem] mb-[1rem] justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
+            About Section
         </div>
     );
 };
