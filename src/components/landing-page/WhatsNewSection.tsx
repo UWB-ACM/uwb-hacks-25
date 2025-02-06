86001b41a83293508e4cb9f2074d2a052007ef93
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-02-06 10:32:14 -0800

    fixed lint error

--- src/components/landing-page/WhatsNewSection.tsx
+++ src/components/landing-page/WhatsNewSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WhatsNewSection = () => {
   return (
       <div className="border-black border-[0.25rem] p-[10rem] mb-[0.5rem]justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
-          What's New?
+          {`What's New?`}
       </div>
   );
 }
