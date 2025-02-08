bf0e0644f817c67b4f372b5b8d68cf31304fcc41
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-02-07 23:53:43 -0800

    gradient

--- src/components/landing-page/HeroSection.tsx
+++ src/components/landing-page/HeroSection.tsx
@@ -5,19 +5,14 @@ import Countdown from "@/src/components/landing-page/(HeroSectionComponents)/Cou
 
 const HeroSection = () => {
     return (
-        <div className="relative min-h-screen bg-gradient-to-b from-purple-300 from-[10%] via-orange-400 via-50% to-red-500 to-80%">
-            {/* City Background */}
-
-            {/* Content */}
+        <div className="relative w-full min-h-screen bg-gradient-to-b from-purple-300 from-[10%] via-orange-400 via-50% to-red-500 to-80%">
             <div className="relative z-10 container px-4 py-8">
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-                    {/* Logo Section */}
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start">
                     <div className="lg:col-span-1 mt-4">
                         <LogoContainer />
-                    </div>
-
-                    <div className="lg:col-span-1 lg:mt-16">
-                        <VolunteerSection />
+                        <div className="lg:col-span-1 lg:mt-16">
+                            <VolunteerSection />
+                        </div>
                     </div>
 
                     <div className="lg:col-span-1 space-y-8 lg:mt-8">
