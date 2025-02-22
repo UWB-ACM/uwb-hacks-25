e5393fb3878234e6638b91703a579c91996fafd6
NainooG <khush.nainn@gmail.com>
2025-02-22 14:21:03 -0800

    improved sponsors section

--- src/components/landing-page/SponsorsSection.tsx
+++ src/components/landing-page/SponsorsSection.tsx
@@ -1,34 +1,61 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const SponsorsSection = () => {
-  return (
-      <div className="border-black border-[0.25rem] p-[10rem] mb-[1rem] mt-[1rem] justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
-          {/* Sponsors Section */}
-          {/* sponsor grid */}
-          <div className="grid gap-3 md:grid-cols-5 justify-items-center items-center">
-            {/* sponsor logo container, place logo w/ img tag inside of each of these divs */}
-            <div className="flex justify-center items-center p-6 transition">sponsor1</div>
+    return (
+        <div className="bg-[#FBE4A6] border-black border-[0.25rem] p-[5rem] mb-[1rem] mt-[1rem] justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
+            {/* Sponsors Section */}
+            <div className="flex flex-col items-center">
+                <div className="justify-center items-center font-h1 text-[#49B2F8] text-5xl md:text-6xl lg:text-7xl text-center">
+                    UWB HACKS 2025 SPONSORS
+                </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor2</div>
+                {/* sponsor grid */}
+                <div className="grid gap-3 md:grid-cols-5 justify-items-center items-center">
+                    {/* sponsor logo container, place logo w/ img inside of each of these divs */}
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor1" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor3</div>
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor2" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor4</div>
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor3" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor5</div>
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor4" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor6</div>
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor5" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor3</div>
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor6" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor4</div>
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor7" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor5</div>
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor8" width={100} height={100}></Image>
+                    </div>
 
-            <div className="flex justify-center items-center p-6 transition">sponsor6</div>
-          </div>
-      </div>
-  );
-}
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor9" width={100} height={100}></Image>
+                    </div>
 
-export default SponsorsSection
+                    <div className="flex justify-center items-center p-6 transition">
+                        <Image src="/globe.svg" alt="sponsor10" width={100} height={100}></Image>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SponsorsSection;
