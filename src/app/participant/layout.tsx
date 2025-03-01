4b6c5085d013eaa091a6f3b5f2017f62ba5234d6
uellenberg <JonahUellenberg@hotmail.com>
2025-03-01 15:36:36 -0800

    Fix header only working for index

--- src/app/participant/layout.tsx
+++ src/app/participant/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/src/components/header/MainHeader";
 import "@/src/app/globals.css";
+import React from "react";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
