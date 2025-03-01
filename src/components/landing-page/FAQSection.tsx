5a728e345785f6e5f058380a023b536d2d79ef83
uellenberg <JonahUellenberg@hotmail.com>
2025-03-01 14:05:16 -0800

    Run eslint and prettier

--- src/components/landing-page/FAQSection.tsx
+++ src/components/landing-page/FAQSection.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const FAQSection = () => {
-  return (
-      <div className="border-black border-[0.25rem] p-[10rem] mb-[1rem] mt-[1rem] justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
-          FAQ Section
-      </div>
-  );
-}
+    return (
+        <div className="border-black border-[0.25rem] p-[10rem] mb-[1rem] mt-[1rem] justify-center grid sm:grid-cols-2 sm:gap-x-[5rem] sm:flex">
+            FAQ Section
+        </div>
+    );
+};
 
-export default FAQSection
+export default FAQSection;
