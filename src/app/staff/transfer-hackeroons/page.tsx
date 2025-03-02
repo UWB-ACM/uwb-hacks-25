b8214b2af650648e784004090fb685785af79eb1
uellenberg <JonahUellenberg@hotmail.com>
2025-03-01 17:36:34 -0800

    Improve SEO tags

--- src/app/staff/transfer-hackeroons/page.tsx
+++ src/app/staff/transfer-hackeroons/page.tsx
@@ -1,6 +1,11 @@
 import { getSession } from "@/src/util/session";
 import React from "react";
 import TransferHackaroonsPage from "@/src/app/staff/transfer-hackeroons/TransferHackaroonsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Transfer Hackaroons | UWB Hacks 25",
+};
 
 export default async function Page() {
     // TODO: Make this apply to the scanned user, not the staff member.
