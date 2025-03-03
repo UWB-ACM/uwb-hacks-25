19e03dc16e175808ad28a3d30309997cece0c3a7
sam-dp <cloudysaga@gmail.com>
2025-03-03 15:17:32 -0800

    Prettier formatting

--- src/components/landing-page/(MeetTheTeamSectionComponents)/MemberCard.tsx
+++ src/components/landing-page/(MeetTheTeamSectionComponents)/MemberCard.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import Image from "next/image";
 
 interface MemberCardProps {
-    member: { id: string; name: string; role: string; photo: string; link?: string };
+    member: {
+        id: string;
+        name: string;
+        role: string;
+        photo: string;
+        link?: string;
+    };
 }
 
 const MemberCard: React.FC<MemberCardProps> = ({ member }) => {
     const isClickable = !!member.link; // Check if the member has a link
 
     const cardContent = (
-        <div className={`relative flex items-center p-4 space-x-4 border-4 border-black shadow-lg bg-white transition-transform transform group hover:scale-105 cursor-pointer duration-300`}>
+        <div
+            className={`relative flex items-center p-4 space-x-4 border-4 border-black shadow-lg bg-white transition-transform transform group hover:scale-105 cursor-pointer duration-300`}
+        >
             {/* Profile Image */}
             <Image
                 src={member.photo}
