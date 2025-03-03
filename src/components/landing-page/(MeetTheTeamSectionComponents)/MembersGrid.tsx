fed0a83d23aa3ed95ed4e5054465415f4924fcb7
sam-dp <cloudysaga@gmail.com>
2025-03-03 14:35:05 -0800

    Added LinkedIn button functionality

--- src/components/landing-page/(MeetTheTeamSectionComponents)/MembersGrid.tsx
+++ src/components/landing-page/(MeetTheTeamSectionComponents)/MembersGrid.tsx
@@ -5,7 +5,7 @@ import MemberCard from "@/src/components/landing-page/(MeetTheTeamSectionCompone
 import gsap from "gsap";
 
 interface TeamMembersGridProps {
-    members: { id: string; name: string; role: string; photo: string }[];
+    members: { id: string; name: string; role: string; photo: string; link: string }[];
 }
 
 const TeamMembersGrid: React.FC<TeamMembersGridProps> = ({ members }) => {
