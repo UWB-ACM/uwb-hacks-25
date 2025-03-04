7d2614b578f98b753cbffeabf468473bac0ee84d
pgobin <pgobin@uw.edu>
2025-03-03 21:46:18 -0800

    removed button from tracks (used it for testing)

--- src/components/landing-page/TrackSection.tsx
+++ src/components/landing-page/TrackSection.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import Button from "../Button";
 
 const TracksSection = () => {
     return (
         <div className="h-full border-black border-[0.25rem] p-[10rem] mb-[1rem] mt-[1rem] grid place-content-center">
-            <Button
-                onClick={() => {
-                    console.log("clicked");
-                }}
-            >
-                Discord
-            </Button>
+            tracks section
         </div>
     );
 };
