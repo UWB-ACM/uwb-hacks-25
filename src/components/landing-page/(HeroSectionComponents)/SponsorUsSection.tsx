71e0b69ad00fd78a3a00bdde76dc36db4f68e298
uellenberg <JonahUellenberg@hotmail.com>
2025-03-04 22:17:34 -0800

    Fix formatting

--- src/components/landing-page/(HeroSectionComponents)/SponsorUsSection.tsx
+++ src/components/landing-page/(HeroSectionComponents)/SponsorUsSection.tsx
@@ -10,10 +10,10 @@ const Page = () => {
 
     return (
         <div className="bg-white/40 rounded-lg p-6">
-            <h2 className="text-xl font-bold mb-4">
-                Become a Sponsor!
-            </h2>
-            <Button className="text-[23px]" onClick={onClick}>Sponsor Us</Button>
+            <h2 className="text-xl font-bold mb-4">Become a Sponsor!</h2>
+            <Button className="text-[23px]" onClick={onClick}>
+                Sponsor Us
+            </Button>
         </div>
     );
 };
