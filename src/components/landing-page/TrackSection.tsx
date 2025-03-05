939f0a7c7aff3822cec15ad9d4eae6c54d1ffea4
pgobin <pgobin@uw.edu>
2025-03-05 13:05:05 -0800

    gift lid flying off animation

--- src/components/landing-page/TrackSection.tsx
+++ src/components/landing-page/TrackSection.tsx
@@ -4,7 +4,6 @@ import React, { useState, useRef, useEffect } from "react";
 import gsap from "gsap";
 
 const TracksSection = () => {
-    const boxRef = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
     const tlRef = useRef<gsap.core.Timeline>(null);
 
@@ -33,6 +32,13 @@ const TracksSection = () => {
             tlRef.current.pause().clear();
         }
 
+        gsap.to(".lid", {
+            y: "-100vh",
+            x: "70vw",
+            rotate: 160,
+            duration: 0.7,
+        });
+
         gsap.to(".box", {
             y: 0,
             rotateZ: 0,
@@ -54,11 +60,10 @@ const TracksSection = () => {
             </h1>
             <div className="h-[40vh] md:h-[30vh] lg:h-[40vh] max-h-[300px] md:max-h-[350px] flex items-center">
                 <button
-                    ref={boxRef}
                     onClick={handleClick}
                     className="box relative flex flex-col items-center"
                 >
-                    <div className="z-[5] absolute -top-5 flex">
+                    <div className="lid z-[5] absolute -top-5 flex">
                         <div className="flex justify-center items-center w-[40px] h-[50px] bg-black rounded-full translate-x-1 -rotate-45">
                             <div className="w-[75%] h-[75%] bg-white rounded-full" />
                         </div>
@@ -66,10 +71,10 @@ const TracksSection = () => {
                             <div className="w-[75%] h-[75%] bg-white rounded-full" />
                         </div>
                     </div>
-                    <div className="z-10 flex justify-center w-[220px] h-[30px] bg-gray-500 rounded-lg border-2 border-black">
+                    <div className="lid z-10 flex justify-center w-[220px] h-[30px] bg-gray-500 rounded-lg border-2 border-black">
                         <div className="w-[20px] h-full bg-gray-700" />
                     </div>
-                    <div className="flex justify-center w-[200px] h-[150px] bg-gray-400 rounded-bl-lg rounded-br-lg border-x-2 border-b-2 border-black">
+                    <div className="flex justify-center w-[200px] h-[150px] bg-gray-400 rounded-bl-lg rounded-br-lg border-2 border-black">
                         <div className="w-[20px] h-full bg-gray-700" />
                     </div>
                 </button>
@@ -80,7 +85,7 @@ const TracksSection = () => {
                 </p>
             )}
             {isOpen && (
-                <p className="flex items-center text-lg md:text-xl lg:text-2xl text-center font-h2 px-6 pb-4 md:p-8">
+                <p className="content flex items-center text-lg md:text-xl lg:text-2xl text-center font-h2 px-6 pb-4 md:p-8">
                     Tracks will be unveiled on the day of the hackathon!
                 </p>
             )}
