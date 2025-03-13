db6ab7a07f08e885de20ab3d78b44d3208affe71
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-03-12 17:29:28 -0700

    fixed button not opening link to devpost

--- src/components/landing-page/(HeroSectionComponents)/RegisterSection.tsx
+++ src/components/landing-page/(HeroSectionComponents)/RegisterSection.tsx
@@ -1,16 +1,23 @@
 import Button from "@/src/components/Button";
+import { useRouter } from "next/navigation";
 
 const RegisterSection = () => {
+    const router = useRouter();
     return (
         <div className="bg-white/40 rounded-lg p-6">
             <h2 className="text-xl font-bold mb-4">
-                Assemble Your Squad & Answer the Call.
+                Assemble Your Squad & Answer the Call!
             </h2>
-            {/* TODO: Add a link here. */}
-            <Button onClick={() => {}} className="text-[23px]">
-                <a href="https://uwb-hacks-save-the-world.devpost.com/">
-                    Register Here
-                </a>
+            <Button
+                onClick={() =>
+                    window.open(
+                        "https://uwb-hacks-save-the-world.devpost.com/",
+                        "_blank",
+                    )
+                }
+                className="text-[23px]"
+            >
+                Register Here
             </Button>
         </div>
     );
