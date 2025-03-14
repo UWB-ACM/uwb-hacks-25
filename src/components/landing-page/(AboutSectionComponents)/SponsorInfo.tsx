fa511440ea92769cf0590c9e6f5554cf3c43fe69
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-03-14 12:30:54 -0700

    minor changes

--- src/components/landing-page/(AboutSectionComponents)/SponsorInfo.tsx
+++ src/components/landing-page/(AboutSectionComponents)/SponsorInfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../../Button";
-
+import Link from "next/link";
 function SponsorInfo() {
     return (
         <>
@@ -9,22 +9,18 @@ function SponsorInfo() {
                     2025 UWB Hacks: Save the World!
                 </p>
                 <p className="text-center mt-5 mb-5 font-h3">
-                    This year, we are calling all students to unite, innovate,
-                    and create for good– with support from our inaugural
-                    sponsor, Avanade, and seven interdisciplinary campus groups
-                    with ACM as the backbone. Students will be tasked to align
-                    their project to specific tracks (subtopics) and judging
-                    criteria, while embracing the theme: “Save the World.”
-                    Inspired by everyday heroes and the power of technology for
-                    good, we chose our theme to invite students to unleash their
-                    inner hero and make a meaningful impact. This inclusive
-                    event encourages creativity and innovation, welcoming
-                    projects of all skill levels, from no-code to high-code
-                    solutions.
+                    We invite students to unite, innovate, and create for good
+                    with support from Avanade and seven campus groups, led by
+                    ACM. Participants will align projects to tracks under the
+                    theme: “Save the World,” embracing technology’s power for
+                    good. All skill levels are welcome, from no-code to
+                    high-code solutions.
                 </p>
-                <Button className="text-[25px]" onClick={() => {}}>
-                    Learn More
+                <Link href={"https://uwb-hacks-save-the-world.devpost.com"}>
+                <Button className="text-[23px]" onClick={() => {}}>
+                    Register Now
                 </Button>
+                </Link>
             </div>
             <div className="mt-10 grow bg-white border-[3px] border-black flex flex-col px-3 py-5 items-center">
                 <p className="font-bold text-center font-h2 [font-size:calc(0.1rem_+_3vw)]">
