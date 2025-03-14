1f063838736c783c37d08ba1e0e3fe9c4a753419
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-03-14 14:24:48 -0700

    added month of hacking page

--- src/app/month-of-hacking/page.tsx
+++ src/app/month-of-hacking/page.tsx
@@ -4,7 +4,11 @@ import React from "react";
 function Page() {
     return (
         <div className="p-10 w-screen flex justify-center">
-            <img src="/monthOfHacking/mohschedule.png" className="max-w-[1200px] rounded-md" alt="image of schedule" />
+            <img
+                src="/monthOfHacking/mohschedule.png"
+                className="max-w-[1200px] rounded-md"
+                alt="image of schedule"
+            />
         </div>
     );
 }
