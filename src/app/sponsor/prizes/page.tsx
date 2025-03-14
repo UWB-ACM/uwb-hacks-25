0a6b64eb67d4788d42621489fa087b5637886339
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-03-14 14:49:12 -0700

    added prizes section

---
+++ src/app/sponsor/prizes/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import Button from "@/src/components/Button";
+import Link from "next/link";
+function Page() {
+    return (
+        <div className="flex flex-col w-full items-center px-6 md:px-12 py-12">
+            <div className="max-w-[1000px] flex flex-col items-center text-center">
+                <h1 className="font-san-marino-beach text-5xl text-[#2886c4] pb-8">
+                    Sponsor Prizes
+                </h1>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full mb-8">
+                    <div className="bg-white border-2 border-black px-8 py-4 shadow-lg rounded-xl hover:shadow-2xl transition-shadow duration-300">
+                        <h3 className="font-rubik-mono font-bold text-2xl text-[#2886c4]">
+                            Sponsored Prizes
+                        </h3>
+                        <p className="font-h3 text-base leading-6 mt-4">
+                            {`Support our hackathon by sponsoring prizes that
+                            reward top talent in business, biotech, and
+                            entertainment. Your sponsorship inspires creativity,
+                            drives innovation, and gives participants exposure
+                            to your organization's mission.`}
+                        </p>
+                    </div>
+
+                    <div className="bg-white border-2 border-black px-8 py-4 shadow-lg rounded-xl hover:shadow-2xl transition-shadow duration-300">
+                        <h3 className="font-rubik-mono font-bold text-2xl text-[#2886c4]">
+                            Why Sponsor a Prize?
+                        </h3>
+                        <p className="font-h3 text-base leading-6 mt-4">
+                            Sponsoring a prize boosts your brand, connects you
+                            with emerging talent, and fuels groundbreaking
+                            ideas. Your support fosters competition,
+                            collaboration, and real-world impact. Let me know if
+                            you need further tweaks!
+                        </p>
+                    </div>
+                </div>
+
+                <Link
+                    href={
+                        "https://docs.google.com/forms/d/e/1FAIpQLSeE5tYU3c8GZ20H1PPZS2-yjZ89tROgPMQRtsybsJ6j10iv4A/viewform?usp=sharing"
+                    }
+                >
+                    <Button onClick={() => {}} className="text-[18px]">
+                        Contact Us Here!
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Page;
