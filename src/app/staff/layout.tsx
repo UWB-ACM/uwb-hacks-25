1a6f5b3b68aec46bdf3298c7a683070e0744eb0c
pgobin <pgobin@uw.edu>
2025-03-14 12:46:57 -0700

    commented out staff header for now

--- src/app/staff/layout.tsx
+++ src/app/staff/layout.tsx
@@ -13,30 +13,31 @@ export default function RootLayout({
 }>) {
     return (
         <div className={`w-screen`}>
-            <Header
-            // navItems={[
-            //     {
-            //         id: "prizes",
-            //         name: "Buy Prizes",
-            //         url: "/staff/buy-prizes",
-            //     },
-            //     {
-            //         id: "modify-user",
-            //         name: "Modify User",
-            //         url: "/staff/modify-user",
-            //     },
-            //     {
-            //         id: "transfer-hackeroons",
-            //         name: "Transfer Hackeroons",
-            //         url: "/staff/transfer-hackeroons",
-            //     },
-            //     {
-            //         id: "dashboard",
-            //         name: "Dashboard",
-            //         url: "/staff",
-            //     },
-            // ]}
-            />
+            {/* IMPLEMENT !!!!! */}
+            {/* <Header
+            navItems={[
+                {
+                    id: "prizes",
+                    name: "Buy Prizes",
+                    url: "/staff/buy-prizes",
+                },
+                {
+                    id: "modify-user",
+                    name: "Modify User",
+                    url: "/staff/modify-user",
+                },
+                {
+                    id: "transfer-hackeroons",
+                    name: "Transfer Hackeroons",
+                    url: "/staff/transfer-hackeroons",
+                },
+                {
+                    id: "dashboard",
+                    name: "Dashboard",
+                    url: "/staff",
+                },
+            ]}
+            /> */}
 
             {children}
         </div>
