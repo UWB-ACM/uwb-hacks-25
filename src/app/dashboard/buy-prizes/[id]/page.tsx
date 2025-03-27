e39f0140ead2a8677d5308d538a3a63466393784
uellenberg <JonahUellenberg@hotmail.com>
2025-03-27 12:24:26 -0700

    Restructure dashboards and add selected user page

--- src/app/dashboard/buy-prizes/[id]/page.tsx
+++ src/app/dashboard/buy-prizes/[id]/page.tsx
@@ -4,7 +4,10 @@ import { getSession } from "@/src/util/session";
 import BuyPage from "@/src/components/staff/BuyPage";
 import { getBalanceForUser } from "@/src/util/db/transaction";
 import { Metadata } from "next";
-import { ensureStaffPermission, extractStaffUserData } from "@/src/util/staff";
+import {
+    ensureStaffPermission,
+    extractDashboardUserData,
+} from "@/src/util/staff";
 
 export const metadata: Metadata = {
     title: "Buy Prizes | UWB Hacks 25",
@@ -20,7 +23,7 @@ export default async function Page({
 
     const [prizes, { user, balance }] = await Promise.all([
         getPrizes(),
-        extractStaffUserData((await params).id).then((user) =>
+        extractDashboardUserData((await params).id).then((user) =>
             getBalanceForUser(user.id).then((balance) => ({
                 user,
                 balance,
---
+++ src/components/dashboards/StaffActions.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function StaffActions() {
+    // TODO: Are there any places where staff need to do something that isn't user-related?
+    return <></>;
+}
+
+export default StaffActions;
