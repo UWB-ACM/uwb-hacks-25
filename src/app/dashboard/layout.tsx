32d52220530d404d27e64d40c74291400ca332be
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-03-26 18:24:33 -0700

    the code works i think

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "@/src/components/header/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Staff Dashboard | UWB Hacks 25",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className={`w-screen`}>
+            <Header
+                links={[
+                    {
+                        id: "logout",
+                        name: "Logout",
+                        url: "/dashboard",
+                    }
+                ]}
+            />
+
+            {children}
+        </div>
+    );
+}
