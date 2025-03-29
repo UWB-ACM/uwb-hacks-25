bb29d19bcee8b1a2286eb4d3042e21457bcac220
sam-dp <cloudysaga@gmail.com>
2025-03-28 19:12:33 -0700

    Fix multiple awaits on cookieStore

--- src/util/logout.ts
+++ src/util/logout.ts
@@ -8,11 +8,11 @@ import { buildKey } from "@/src/util/redis";
  * Function to log out a user by deleting their session from Redis and clearing their session cookie. Redirection handled in handleLogout().
  */
 export async function logoutUser() {
-    const cookieStore = cookies();
-    const sessionCookie = (await cookieStore).get("session-uwbh25");
+    const cookieStore = await cookies();
+    const sessionCookie = (cookieStore).get("session-uwbh25");
 
     if (sessionCookie?.value) {
-        await redis.del(buildKey("session", sessionCookie.value));
+        redis.del(buildKey("session", sessionCookie.value));
 
         (await cookieStore).set({
             name: "session-uwbh25",
