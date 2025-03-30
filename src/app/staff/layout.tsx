3d575b13ff218959fe8f64a7864e4bcb6b76a122
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-03-30 15:07:38 -0700

    User Dashboard: Profile section and Transaction History Table

---
+++ src/app/staff/layout.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Header from "@/src/components/header/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Staff Dashboard | UWB Hacks 25",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className={`w-screen`}>
+            <Header
+                links={[
+                    {
+                        id: "prizes",
+                        name: "Buy Prizes",
+                        url: "/staff/buy-prizes",
+                    },
+                    {
+                        id: "modify-user",
+                        name: "Modify User",
+                        url: "/staff/modify-user",
+                    },
+                    {
+                        id: "transfer-hackeroons",
+                        name: "Transfer Hackeroons",
+                        url: "/staff/transfer-hackeroons",
+                    },
+                    {
+                        id: "dashboard",
+                        name: "Dashboard",
+                        url: "/staff",
+                    },
+                ]}
+            />
+
+            {children}
+        </div>
+    );
+}
--- src/components/dashboards/UserDashboard.tsx
+++ src/components/dashboards/UserDashboard.tsx
@@ -5,41 +5,171 @@ import {
     getTransactionsForUser,
 } from "@/src/util/db/transaction";
 import QRCode from "react-qr-code";
+import Image from "next/image";
+import Husky from "@/public/about/husky.png";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "../ui/table";
 
 async function UserDashboard({ user }: { user: SessionUser }) {
+
+    const transactions = await getTransactionsForUser(user.id);
+
+    const dummyTransactions = [
+        {
+            id: 1,
+            type: 0,
+            amount: 100,
+            event: "ai workshop",
+            prize: "pat on the head",
+            time: "4/27/2025",
+        },
+        {
+            id: 2,
+            type: 0,
+            amount: 100,
+            event: "ai workshop",
+            prize: "pat on the head",
+            time: "4/27/2025",
+        },
+        {
+            id: 3,
+            type: 0,
+            amount: 100,
+            event: "ai workshop",
+            prize: "pat on the head",
+            time: "4/27/2025",
+        },
+    ];
     return (
-        <div className="pt-20 px-[5%]">
-            <div className="flex pb-10">
-                {/* Profile heading */}
-                <div className="rounded-full w-20 h-20 bg-[lightgray] text-center">
-                    pfp
+        // User Dashboard Container
+        <div className="w-[90%] mx-auto rounded-md">
+            {/* General User Information */}
+            <div className="w-full flex flex-col gap-y-8 md:flex-row justify-around items-center md:px-14 lg:px-20">
+                {/* User PFP, First and Last Name, & Hackeroon Amount */}
+                <div className="flex gap-x-8">
+                    <Image
+                        src={user.picture || Husky}
+                        alt={"User profile photo"}
+                        width={150}
+                        height={150}
+                        className="rounded-full"
+                    />
+                    <div className="flex flex-col justify-around py-2">
+                        <div>
+                            <div className="text-2xl font-bold">
+                                {user.name}
+                            </div>
+                            <div className="text-lg ">
+                                {"User ID: " + user.id}
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-x-2 text-xl">
+                            <p>Hackeroons: </p>
+                            <span className="font-bold text-3xl">
+                                {await getBalanceForUser(user.id)}
+                            </span>
+                        </div>
+                    </div>
                 </div>
-                <div className="ml-4 pr-24">
-                    <div className="mb-1">{user.name}</div>
-                    <div>Hackeroons: {await getBalanceForUser(user.id)}</div>
-                </div>
-                {/* QR code */}
+
+                {/* User Unique QR Code */}
                 <div>
-                    <Suspense>
+                    <Suspense
+                        fallback={
+                            <div className="w-[150px] h-[150px] border-[1px] border-black bg-gray-400" />
+                        }
+                    >
                         <QRCode
-                            size={64}
+                            size={150}
                             value={`https://uwbhacks.com/dashboard/${user.id}`}
                             viewBox={`0 0 256 256`}
                         />
                     </Suspense>
+                    <h3 className="text-center text-lg mt-2">Your QR Code</h3>
                 </div>
             </div>
 
-            {/* Hackeroon history */}
-            <div>
-                <div>Transaction History</div>
-                {(await getTransactionsForUser(user.id)).map((transaction) => (
-                    <div key={transaction.id}>
-                        [{transaction.type}] H${transaction.amount}{" "}
-                        {transaction.event} {transaction.prize}{" "}
-                        {transaction.time.toString()}
-                    </div>
-                ))}
+            {/* User Transaction History */}
+
+            {/*
+            id: 3,
+            type: 0,
+            amount: 100,
+            event: "ai workshop",
+            prize: "pat on the head",
+            time: "4/27/2025",
+            */}
+            <div className="rounded-xl border border-gray-300 overflow-hidden shadow-sm mt-[2rem]">
+                <h3 className="bg-white p-3 font-bold">
+                    {" "}
+                    Transaction History{" "}
+                </h3>
+                <div className="overflow-x-auto">
+                    <Table className="w-full bg-white">
+                        <TableCaption className="mt-0 p-2 text-gray-600 bg-gray-100 border-t border-gray-300">
+                            A list of your recent transactions.
+                        </TableCaption>
+                        <TableHeader>
+                            <TableRow className="bg-[#f3f3f3] hover:bg-[#f3f3f3]">
+                                <TableHead className="px-4 py-2 font-medium text-gray-500  border border-gray-300 whitespace-nowrap">
+                                    Type
+                                </TableHead>
+                                <TableHead className="px-4 py-2 font-medium text-gray-500  border border-gray-300 whitespace-nowrap">
+                                    Amount
+                                </TableHead>
+                                <TableHead className="px-4 py-2 font-medium text-gray-500  border border-gray-300 whitespace-nowrap">
+                                    Event Name
+                                </TableHead>
+                                <TableHead className="px-4 py-2 font-medium text-gray-500  border border-gray-300 whitespace-nowrap">
+                                    Prize
+                                </TableHead>
+                                <TableHead className="px-4 py-2 font-medium text-gray-500  border border-gray-300 whitespace-nowrap">
+                                    Time
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {transactions.map((transaction, index) => (
+                                <TableRow
+                                    key={transaction.id}
+                                    className={
+                                        index % 2 === 0
+                                            ? "bg-white hover:bg-gray-100"
+                                            : "bg-[#f8f8f8] hover:bg-gray-100"
+                                    }
+                                >
+                                    <TableCell className="px-4 py-2 border border-gray-300">
+                                        {transaction.type}
+                                    </TableCell>
+                                    <TableCell className="px-4 py-2 border border-gray-300">
+                                        {transaction.amount}
+                                    </TableCell>
+                                    <TableCell className="px-4 py-2 border border-gray-300">
+                                        {transaction.event !== null
+                                            ? transaction.event
+                                            : "No event associated with this transaction"}
+                                    </TableCell>
+                                    <TableCell className="px-4 py-2 border border-gray-300">
+                                        {transaction.prize !== null
+                                            ? transaction.prize
+                                            : "No prize associated with this transaction"}
+                                    </TableCell>
+                                    <TableCell className="px-4 py-2 border border-gray-300">
+                                        {transaction.time.toLocaleString()}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
         </div>
     );
