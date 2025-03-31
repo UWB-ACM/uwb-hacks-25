cea34c2b0347536bbdfaf1545be293b86021d48f
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2025-03-30 18:01:17 -0700

    Modified getTransactionsForUser to obtain event and prize names using LEFT JOIN

--- src/util/db/transaction.ts
+++ src/util/db/transaction.ts
@@ -135,9 +135,8 @@ export async function getTransactionsForUser(
     user: number,
 ): Promise<Transaction[]> {
     const data =
-        await sql`SELECT id, type, amount, authorized_by, event, prize, time FROM transactions WHERE "user"=${user};`;
-
-
+        await sql`SELECT transactions.id, type, amount, authorized_by, event, prize, time, prizes.name AS prize_name, events.name AS event_name FROM transactions LEFT JOIN prizes ON prizes.id=transactions.prize LEFT JOIN events ON events.id=transactions.event WHERE "user"=${user};`;
+        
     return data.map((row) => ({
         id: row.id,
         user,
@@ -146,6 +145,8 @@ export async function getTransactionsForUser(
         authorized_by: row.authorized_by,
         event: row.event,
         prize: row.prize,
+        eventName: row.event_name, 
+        prizeName: row.prize_name,
         time: row.time,
     }));
 }
