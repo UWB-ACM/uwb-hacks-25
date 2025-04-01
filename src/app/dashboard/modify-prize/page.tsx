d601c9cf6de6daa7af96c30bfb544a95509880cc
uellenberg <JonahUellenberg@hotmail.com>
2025-03-31 17:07:37 -0700

    Fix build

--- src/app/dashboard/modify-prize/page.tsx
+++ src/app/dashboard/modify-prize/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { getPrizes } from "@/src/util/db/prize";
 import PrizeClientWrapper from "./PrizeClientWrapper";
+import { getSession } from "@/src/util/session";
+import { ensureStaffPermission } from "@/src/util/staff";
 
-export default function ModifyPrizePage() {
+export default async function ModifyPrizePage() {
+    const session = await getSession();
+    await ensureStaffPermission(session);
+
     const prizes = getPrizes();
 
     return (
