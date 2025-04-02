bdd4314541a8d7a9e8c1238fe9ed7f9cdbcf3353
uellenberg <JonahUellenberg@hotmail.com>
2025-04-01 19:23:55 -0700

    Add event selector to check-in

--- src/app/dashboard/create-check-in-code/page.tsx
+++ src/app/dashboard/create-check-in-code/page.tsx
@@ -4,6 +4,7 @@ import { getSession } from "@/src/util/session";
 import { Metadata } from "next";
 import { ensureStaffPermission } from "@/src/util/staff";
 import CheckInCodeGenerator from "@/src/components/dashboards/userdashboard/CheckInCodeGenerator";
+import { getEvents } from "@/src/util/db/event";
 
 export const metadata: Metadata = {
     title: "Create Check-In Code | UWB Hacks 25",
@@ -13,6 +14,8 @@ export default async function Page() {
     const session = await getSession();
     await ensureStaffPermission(session);
 
+    const events = getEvents();
+
     // returning the card with user information
     return (
         // centering all the content on the screen
@@ -20,7 +23,9 @@ export default async function Page() {
             {/* the actual card with all the user data */}
             <div className="h-[80vh] w-[90vw] md:w-[60vw] lg:w-[40vw] bg-neutral-100 p-5 flex flex-col justify-between rounded-md border-2 border-black shadow-lg">
                 {/* main content of the user, separate from the exit button */}
-                <div>{session.user && <CheckInCodeGenerator />}</div>
+                <div>
+                    {session.user && <CheckInCodeGenerator events={events} />}
+                </div>
                 {/* exit button that returns user back to staff dashboard */}
                 <Link
                     className="py-3 rounded-lg text-center bg-red-500 text-white"
