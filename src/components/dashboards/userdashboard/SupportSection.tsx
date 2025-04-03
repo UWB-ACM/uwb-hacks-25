43ee6e4ebb7d427dda26e44776a1833e84835268
iteerP <preetigobindpuri02@gmail.com>
2025-04-03 16:11:07 -0700

    fixed href on SupportSection button

--- src/components/dashboards/userdashboard/SupportSection.tsx
+++ src/components/dashboards/userdashboard/SupportSection.tsx
@@ -3,9 +3,17 @@ import Button from "@/src/components/Button";
 import React from "react";
 
 function SupportSection() {
-    // TODO: Add in URL.
-
-    return <Button href={"#"}>Contact Support</Button>;
+    return (
+        <Button
+            href={
+                "https://docs.google.com/forms/d/e/1FAIpQLSeE5tYU3c8GZ20H1PPZS2-yjZ89tROgPMQRtsybsJ6j10iv4A/viewform?usp=sharing"
+            }
+            target={"_"}
+            color="yellow"
+        >
+            Contact Support
+        </Button>
+    );
 }
 
 export default SupportSection;
