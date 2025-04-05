629ef243978d5c15a4138fdadfd0c8f1e1db8ac0
uellenberg <JonahUellenberg@hotmail.com>
2025-04-04 21:30:04 -0700

    Remove redbull and add sponsor URLs

--- src/components/landing-page/SponsorsSection.tsx
+++ src/components/landing-page/SponsorsSection.tsx
@@ -3,7 +3,7 @@ import SponsorBGImage from "@/public/sponsorsSection/sponsor-bg.svg";
 import UWBStemImage from "@/public/sponsorsSection/uwb-stem.png";
 import UWBBusinessImage from "@/public/sponsorsSection/uwb-business.png";
 import AvanadeImage from "@/public/sponsorsSection/avanade-logo.svg";
-import RedBullImage from "@/public/sponsorsSection/redbull-logo.svg";
+import Link from "next/link";
 
 const SponsorsSection = () => {
     return (
@@ -23,39 +23,49 @@ const SponsorsSection = () => {
                 {/* Main sponsors row */}
                 <div className="w-full flex flex-wrap justify-center items-center gap-8 md:gap-16 mb-16">
                     <div className="flex justify-center items-center">
-                        <Image
-                            src={UWBStemImage}
-                            width={300}
-                            height={100}
-                            alt="UW Bothell School of STEM"
-                            className="object-contain"
-                        />
+                        <Link href="https://www.uwb.edu/stem/" target="_blank">
+                            <Image
+                                src={UWBStemImage}
+                                width={300}
+                                height={100}
+                                alt="UW Bothell School of STEM"
+                                className="object-contain"
+                            />
+                        </Link>
                     </div>
 
                     <div className="flex justify-center items-center">
-                        <Image
-                            src={AvanadeImage}
-                            width={350}
-                            height={100}
-                            alt="Avanade"
-                            className="object-contain"
-                        />
+                        <Link href="https://www.avanade.com" target="_blank">
+                            <Image
+                                src={AvanadeImage}
+                                width={350}
+                                height={100}
+                                alt="Avanade"
+                                className="object-contain"
+                            />
+                        </Link>
                     </div>
 
                     <div className="flex justify-center items-center">
-                        <Image
-                            src={UWBBusinessImage}
-                            width={300}
-                            height={100}
-                            alt="UW Bothell School of Business"
-                            className="object-contain"
-                        />
+                        <Link
+                            href="https://www.uwb.edu/business/"
+                            target="_blank"
+                        >
+                            <Image
+                                src={UWBBusinessImage}
+                                width={300}
+                                height={100}
+                                alt="UW Bothell School of Business"
+                                className="object-contain"
+                            />
+                        </Link>
                     </div>
                 </div>
 
                 {/* Secondary sponsors row */}
                 <div className="w-full flex justify-center items-center">
                     <div className="flex justify-center items-center">
+                        {/*
                         <Image
                             src={RedBullImage}
                             width={100}
@@ -63,6 +73,7 @@ const SponsorsSection = () => {
                             alt="Red Bull"
                             className="object-contain"
                         />
+                        */}
                     </div>
                 </div>
             </div>
