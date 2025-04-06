5287369474348371827be41c5f4c8bae45435650
iteerP <preetigobindpuri02@gmail.com>
2025-04-06 16:02:53 -0700

    tracks / content land animation

--- src/components/landing-page/TrackSection.tsx
+++ src/components/landing-page/TrackSection.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import gsap from "gsap";
 
 // Panel components
 import Panel from "./(AboutSectionComponents)/Panel/Panel";
@@ -17,6 +20,19 @@ const TracksSection = () => {
     const mysteryBoxContents =
         mystery_box_contents_data["contents"]["preHackathonContents"];
 
+    const [showTracks, setShowTracks] = useState(false);
+
+    useEffect(() => {
+        if (!showTracks) return;
+        gsap.set(".tracks", { y: "-100vh" });
+        gsap.to(".tracks", {
+            delay: 0.25,
+            y: 0,
+            stagger: 0.25,
+            ease: "power1.out",
+        });
+    }, [showTracks]);
+
     return (
         <Panel className="mt-12 md:mt-16" panelColor="white">
             <PanelHeader
@@ -27,15 +43,22 @@ const TracksSection = () => {
                 Tracks
             </PanelHeader>
             <PanelContent className="relative flex flex-col items-center">
-                <MysteryBox contents={mysteryBoxContents} />
-                {mysteryBoxContents.map((content, idx) => (
-                    <p
-                        key={idx}
-                        className="font-h1 text-xl md:text-2xl lg:text-3xl md:w-[70%] text-center mt-6"
-                    >
-                        {content}
-                    </p>
-                ))}
+                <MysteryBox
+                    contents={mysteryBoxContents}
+                    setShowTracks={setShowTracks}
+                />
+                {showTracks && (
+                    <>
+                        {mysteryBoxContents.map((content, idx) => (
+                            <p
+                                key={idx}
+                                className="tracks font-h1 text-xl md:text-2xl lg:text-3xl md:w-[70%] text-center mt-6"
+                            >
+                                {content}
+                            </p>
+                        ))}
+                    </>
+                )}
             </PanelContent>
         </Panel>
     );
