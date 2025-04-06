2b6e34d5e6e659e29672e7fe8005ccdc059929a0
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-04-05 18:57:46 -0700

    added banner for live updates

---
+++ src/components/header/DesktopBanner.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+type BannerProps = {
+    bannerVisible: boolean;
+    setBannerVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function DesktopBanner({bannerVisible, setBannerVisible} : BannerProps) {
+
+    return (
+        <>
+        {bannerVisible && (
+            <div className="bg-orange-500 text-lg text-white font-rubik-mono-one justify-center items-center flex h-10">
+                <p>Month of Hacking is live!</p>
+                <div className="w-4"></div>
+
+                <Link href={"/month-of-hacking"} className="text-white font-bold hover:underline">
+                    <p className="underline cursor-pointer">Learn More</p>
+                </Link>
+                <button className="absolute right-10" onClick={() => setBannerVisible(false)}>X</button>
+            </div>
+        )}
+       </>
+    );
+}
+
+export default DesktopBanner;
---
+++ src/components/header/MobileBanner.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+type BannerProps = {
+    bannerVisible: boolean;
+    setBannerVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function MobileBanner({bannerVisible, setBannerVisible} : BannerProps) {
+
+    return (
+        <>
+        {bannerVisible && (
+            <div className="bg-orange-500 text-[9px] text-white font-rubik-mono-one justify-center items-center flex flex-col h-14">
+                <p>Month of Hacking is live!</p>
+                <div className="w-4"></div>
+
+                <Link href={"/month-of-hacking"} className="text-white font-bold hover:underline">
+                    <p className="underline cursor-pointer">Learn More</p>
+                </Link>
+            </div>
+        )}
+       </>
+    );
+}
+
+export default MobileBanner;
