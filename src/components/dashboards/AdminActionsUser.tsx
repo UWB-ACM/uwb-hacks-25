7b9c60d151776c7a26d8e9fbe67a0bd8e972e3d3
uellenberg <JonahUellenberg@hotmail.com>
2025-04-06 11:41:49 -0700

    Clean up staff actions

--- src/components/dashboards/AdminActionsUser.tsx
+++ src/components/dashboards/AdminActionsUser.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { User } from "@/src/util/dataTypes";
-import { FaGift } from "react-icons/fa6";
-import DashboardButton from "@/src/components/dashboards/DashboardButton";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function AdminActionsUser({ user }: { user: User }) {
     // TODO: Make these actual actions.
     return (
         <>
-            <DashboardButton href="#">
+            {/*<DashboardButton href="#">
                 <FaGift className="h-[30px] w-[30px]" />
                 Modify User
-            </DashboardButton>
+            </DashboardButton>*/}
         </>
     );
 }
