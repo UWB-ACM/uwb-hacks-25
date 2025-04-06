7639695996e16b646a9a569b21a35b28c06b9d68
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-04-05 17:37:10 -0700

    initial commit

---
+++ src/components/header/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className="bg-blue-500 text-white p-4 justify-between flex">
+        <h1>Text</h1>
+        <button>Learn More</button>
+    </div>
+  )
+}
+
+export default Banner
