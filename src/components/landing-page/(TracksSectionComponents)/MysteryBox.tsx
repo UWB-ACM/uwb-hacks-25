b4e1482979f628342937e6605dc04beef362143e
pgobin <pgobin@uw.edu>
2025-04-06 15:08:53 -0700

    added contents for mystery box

--- src/components/landing-page/(TracksSectionComponents)/MysteryBox.tsx
+++ src/components/landing-page/(TracksSectionComponents)/MysteryBox.tsx
@@ -14,7 +14,11 @@ import {
     popLid,
 } from "./MysteryBoxAnimationFunctions";
 
-export default function MysteryBox() {
+type MysteryBoxProps = {
+    contents: string[];
+};
+
+export default function MysteryBox({ contents }: MysteryBoxProps) {
     const tlRef = useRef<gsap.core.Timeline | null>(null);
     const mysteryBoxRef = useRef<HTMLButtonElement | null>(null);
     const mysteryBoxLidRef = useRef<SVGSVGElement | null>(null);
@@ -63,12 +67,15 @@ export default function MysteryBox() {
             onClick={handleBoxOpen}
             className="relative"
         >
-            <MysteryBoxContainer
-                className="w-[200px] md:w-[325px] lg:w-[375px] h-fit"
-                mysteryBoxContents={[
-                    "All hackathon tracks will be unveiled on the day of the hackathon!",
-                ]}
-            />
+            {contents.map((content, idx) => (
+                <p
+                    key={idx}
+                    className="-z-[5] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 scale-75 font-h1 text-xl md:text-2xl lg:text-3xl text-center"
+                >
+                    {content}
+                </p>
+            ))}
+            <MysteryBoxContainer className="w-[200px] md:w-[325px] lg:w-[375px] h-fit" />
             <MysteryBoxLid
                 ref={mysteryBoxLidRef}
                 className="absolute top-0 w-[200px] md:w-[325px] lg:w-[375px] h-fit"
--- src/components/landing-page/TrackSection.tsx
+++ src/components/landing-page/TrackSection.tsx
@@ -5,12 +5,18 @@ import Panel from "./(AboutSectionComponents)/Panel/Panel";
 import PanelContent from "./(AboutSectionComponents)/Panel/Content";
 import PanelHeader from "./(AboutSectionComponents)/Panel/Header";
 
+// Mystery Box components / assets
 import MysteryBox from "./(TracksSectionComponents)/MysteryBox";
+import mystery_box_contents_data from "./(TracksSectionComponents)/mysteryBoxContents.json";
 
 // Used in Tracks section background
 import QuestionMark from "./(TracksSectionComponents)/QuestionMark";
 
 const TracksSection = () => {
+    // CHANGE FROM "preHackathonContents" --> "duringHackathonContents" to reveal track names
+    const mysteryBoxContents =
+        mystery_box_contents_data["contents"]["preHackathonContents"];
+
     return (
         <Panel className="mt-12 md:mt-16" panelColor="white">
             <PanelHeader
@@ -21,7 +27,7 @@ const TracksSection = () => {
                 Tracks
             </PanelHeader>
             <PanelContent className="relative flex flex-col items-center">
-                <MysteryBox />
+                <MysteryBox contents={mysteryBoxContents} />
                 <p className="font-h1 text-xl md:text-2xl lg:text-3xl md:w-[70%] text-center mt-6">
                     All hackathon tracks will be unveiled on the day of the
                     hackathon!
