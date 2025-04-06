6fdc7a84397cde855d525fe0bfdaa75aef0a2adf
Jonah Uellenberg <JonahUellenberg@hotmail.com>
2025-04-05 22:25:58 -0700

    Remove test data from transactions table

--- src/components/dashboards/userdashboard/TransactionTable.tsx
+++ src/components/dashboards/userdashboard/TransactionTable.tsx
@@ -17,48 +17,6 @@ type TransactionTableProps = {
 
 const TransactionTable = ({ transactions }: TransactionTableProps) => {
     const [showMore, setShowMore] = useState(false);
-    const testTransactions_MoreThanThreeTransactions = [
-        {
-            id: 14,
-            type: 1,
-            amount: 125,
-            eventName: "test event 1",
-            prizeName: "test prize 1",
-            time: "4/4/2025, 2:00:00 PM",
-        },
-        {
-            id: 15,
-            type: 2,
-            amount: 15,
-            eventName: "test event 2",
-            prizeName: "test prize 2",
-            time: "4/4/2025, 4:00:00 PM",
-        },
-        {
-            id: 16,
-            type: 1,
-            amount: 25,
-            eventName: "test event 3",
-            prizeName: "test prize 3",
-            time: "4/4/2025, 5:00:00 PM",
-        },
-        {
-            id: 17,
-            type: 2,
-            amount: 12,
-            eventName: "test event 4",
-            prizeName: "test prize 4",
-            time: "4/4/2025, 6:00:00 PM",
-        },
-        {
-            id: 18,
-            type: 1,
-            amount: 5,
-            eventName: "test event 5",
-            prizeName: "test prize 5",
-            time: "4/4/2025, 8:00:00 PM",
-        },
-    ];
 
     return (
         <div className="rounded-xl border border-gray-300 overflow-hidden shadow-sm mt-[2rem]">
