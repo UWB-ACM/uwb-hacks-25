78e2bcb42ddb9323f664a0e7863f6ba476925eef
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-04-05 19:02:41 -0700

    ran prettier

--- src/components/header/DesktopBanner.tsx
+++ src/components/header/DesktopBanner.tsx
@@ -6,22 +6,29 @@ type BannerProps = {
     setBannerVisible: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-function DesktopBanner({bannerVisible, setBannerVisible} : BannerProps) {
-
+function DesktopBanner({ bannerVisible, setBannerVisible }: BannerProps) {
     return (
         <>
-        {bannerVisible && (
-            <div className="bg-orange-500 text-lg text-white font-rubik-mono-one justify-center items-center flex h-10">
-                <p>Month of Hacking is live!</p>
-                <div className="w-4"></div>
+            {bannerVisible && (
+                <div className="bg-orange-500 text-lg text-white font-rubik-mono-one justify-center items-center flex h-10">
+                    <p>Month of Hacking is live!</p>
+                    <div className="w-4"></div>
 
-                <Link href={"/month-of-hacking"} className="text-white font-bold hover:underline">
-                    <p className="underline cursor-pointer">Learn More</p>
-                </Link>
-                <button className="absolute right-10" onClick={() => setBannerVisible(false)}>X</button>
-            </div>
-        )}
-       </>
+                    <Link
+                        href={"/month-of-hacking"}
+                        className="text-white font-bold hover:underline"
+                    >
+                        <p className="underline cursor-pointer">Learn More</p>
+                    </Link>
+                    <button
+                        className="absolute right-10"
+                        onClick={() => setBannerVisible(false)}
+                    >
+                        X
+                    </button>
+                </div>
+            )}
+        </>
     );
 }
 
