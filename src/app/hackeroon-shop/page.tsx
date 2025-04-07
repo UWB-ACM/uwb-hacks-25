2b55f49d3c50d87062be6a637389ea1be2471531
iteerP <preetigobindpuri02@gmail.com>
2025-04-07 14:23:14 -0700

    small edit

--- src/app/hackeroon-shop/page.tsx
+++ src/app/hackeroon-shop/page.tsx
@@ -84,8 +84,7 @@ export default async function PrizesPage() {
             </div>
 
             <p className="self-center text-center max-w-[80%] md:max-w-[50%] font-bold">
-                Prizes are offered on a first-come-first-serve basis, and can be
-                purchased at our prize stand in-person [INSERT LOCATION].
+                Prizes are offered on a first-come-first-serve basis
             </p>
 
             <div className="w-full flex-grow p-8 overflow-scroll overflow-x-hidden grid md:grid-cols-2 lg:grid-cols-3 gap-6">
