fff643131c50596cb5ddfc40fd544be98359b259
pgobin <pgobin@uw.edu>
2025-04-06 20:56:14 -0700

    refactored MysteryBoxLid comp to use standalone svg

--- src/components/landing-page/(TracksSectionComponents)/QuestionMark.tsx
+++ src/components/landing-page/(TracksSectionComponents)/QuestionMark.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import clsx from "clsx";
-import Image from "next/image";
 
 type QuestionMarkProps = {
     className?: string;
