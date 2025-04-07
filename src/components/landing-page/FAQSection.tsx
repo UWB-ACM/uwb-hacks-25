1d93018e4cc88c4c35a83af9a2bd2747a10b1758
iteerP <preetigobindpuri02@gmail.com>
2025-04-07 15:07:05 -0700

    space out FAQ Questions

--- src/components/landing-page/FAQSection.tsx
+++ src/components/landing-page/FAQSection.tsx
@@ -16,64 +16,72 @@ const FAQSection = () => {
                 FAQ
             </PanelHeader>
 
-            <PanelContent>
-                <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
-                    1. How will UWB Hacks 2025 be run?
-                </h3>
-                <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
-                    The hackathon will take place over a weekend, where
-                    participants will form teams, brainstorm ideas, and build
-                    projects that align with this year’s theme: Save the World
-                    You will have access to mentors, workshops, and networking
-                    opportunities to help bring your ideas to life.
-                </p>
-
-                <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
-                    2. Do I have to be a UWB student to participate?
-                </h3>
-                <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
-                    No! UWB Hacks 2025 is open to students from any university
-                    and high school students as well.
-                </p>
-
-                <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
-                    3. Does it cost money to enter the hackathon?
-                </h3>
-                <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
-                    No! Participation is completely free.
-                </p>
-
-                <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
-                    4. Do I need programming experience to participate?
-                </h3>
-                <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
-                    No prior programming experience is required! We welcome
-                    participants of all skill levels and will provide workshops,
-                    mentorship, and other learning opportunities.
-                </p>
-
-                <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
-                    5. Do I need a team to register, and how many people can be
-                    on a team?
-                </h3>
-                <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
-                    Yes. If you don’t have a team, we will provide opportunities
-                    to form teams before the hackathon begins. Each team must
-                    have 3 to 5 members—no exceptions. Teams that don’t meet
-                    this requirement will be disqualified.
-                </p>
-
-                <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
-                    6. What activities will take place aside from hacking?
-                </h3>
-                <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
-                    {`In addition to the hackathon, participants will have the
+            <PanelContent className="flex flex-col gap-y-3">
+                <div>
+                    <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
+                        1. How will UWB Hacks 2025 be run?
+                    </h3>
+                    <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
+                        The hackathon will take place over a weekend, where
+                        participants will form teams, brainstorm ideas, and
+                        build projects that align with this year’s theme: Save
+                        the World You will have access to mentors, workshops,
+                        and networking opportunities to help bring your ideas to
+                        life.
+                    </p>
+                </div>
+                <div>
+                    <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
+                        2. Do I have to be a UWB student to participate?
+                    </h3>
+                    <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
+                        No! UWB Hacks 2025 is open to students from any
+                        university and high school students as well.
+                    </p>
+                </div>
+                <div>
+                    <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
+                        3. Does it cost money to enter the hackathon?
+                    </h3>
+                    <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
+                        No! Participation is completely free.
+                    </p>
+                </div>
+                <div>
+                    <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
+                        4. Do I need programming experience to participate?
+                    </h3>
+                    <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
+                        No prior programming experience is required! We welcome
+                        participants of all skill levels and will provide
+                        workshops, mentorship, and other learning opportunities.
+                    </p>
+                </div>
+                <div>
+                    <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
+                        5. Do I need a team to register, and how many people can
+                        be on a team?
+                    </h3>
+                    <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
+                        Yes. If you don’t have a team, we will provide
+                        opportunities to form teams before the hackathon begins.
+                        Each team must have 3 to 5 members—no exceptions. Teams
+                        that don’t meet this requirement will be disqualified.
+                    </p>
+                </div>
+                <div>
+                    <h3 className="font-rubik-mono-one text-md md:text-lg font-stretch-condensed">
+                        6. What activities will take place aside from hacking?
+                    </h3>
+                    <p className="ml-4 md:pl-12 font-h3 text-sm md:text-base leading-6">
+                        {`In addition to the hackathon, participants will have the
                     opportunity to attend interactive workshops, networking
                     sessions, and engaging activities. These will include
                     insightful talks from industry professionals and hands-on
                     learning experiences designed to align with this year's Save
                     the World theme.`}
-                </p>
+                    </p>
+                </div>
             </PanelContent>
         </Panel>
     );
