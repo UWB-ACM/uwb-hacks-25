d7b75cf46ad612269930667079eb95cea314e119
iteerP <preetigobindpuri02@gmail.com>
2025-04-13 19:28:24 -0700

    adjusted gap between LeaderboardRecord components

--- src/components/landing-page/(AboutSectionComponents)/leaderboard/LeaderboardRecord.tsx
+++ src/components/landing-page/(AboutSectionComponents)/leaderboard/LeaderboardRecord.tsx
@@ -16,7 +16,7 @@ export default function LeaderboardUserRecord({
 }: LeaderboardUserRecordProps) {
     return (
         <div className="grid grid-cols-3 gap-x-3 md:gap-x-6 font-h1 bg-white border-2 border-black p-3 text-black md:text-lg lg:text-xl rounded-tr-md rounded-bl-md shadow-[3px_3px_0_rgb(0,0,0,1)]">
-            <div className="flex justify-center items-center gap-x-4">
+            <div className="flex justify-center items-center gap-x-4 md:gap-x-6">
                 <p className="text-2xl md:text-3xl">{ranking}</p>
                 <Image
                     className="rounded-md border border-black"
