3784399c72b5360e10dfa7afce782b22dc09de48
pgobin <pgobin@uw.edu>
2025-04-16 21:36:39 -0700

    styling changes

--- src/components/landing-page/(AboutSectionComponents)/leaderboard/LeaderboardRecord.tsx
+++ src/components/landing-page/(AboutSectionComponents)/leaderboard/LeaderboardRecord.tsx
@@ -15,23 +15,22 @@ export default function LeaderboardUserRecord({
     picture,
 }: LeaderboardUserRecordProps) {
     return (
-        <div className="grid grid-cols-3 gap-x-3 md:gap-x-6 font-h1 bg-white border-2 border-black p-3 text-black md:text-lg lg:text-xl rounded-tr-md rounded-bl-md shadow-[3px_3px_0_rgb(0,0,0,1)]">
-            <div className="flex justify-center items-center gap-x-4 md:gap-x-6">
-                <p className="text-2xl md:text-3xl">{ranking}</p>
-                <Image
-                    className="rounded-md border border-black"
-                    src={picture}
-                    alt="User Profile Photo"
-                    width="75"
-                    height="75"
-                />
+        <div className="grid grid-cols-3 gap-x-2 font-h1 bg-white border-2 border-black p-3 text-black md:text-lg lg:text-xl rounded-tr-md rounded-bl-md shadow-[3px_3px_0_rgb(0,0,0,1)]">
+            <div className="flex gap-x-2 items-center justify-around">
+                <p className="w-fit self-center text-2xl md:text-3xl">
+                    {ranking}
+                </p>
+                <div className="relative w-[50px] h-[50px] md:w-[55px] md:h-[55px] lg:w-[65px] lg:h-[65px] aspect-square">
+                    <Image
+                        className="rounded-md border border-black"
+                        src={picture}
+                        alt="User Profile Photo"
+                        fill
+                    />
+                </div>
             </div>
-            <div className="flex justify-center items-center">
-                <p>{name}</p>
-            </div>
-            <div className="flex justify-center items-center">
-                <p>{hackeroonAmount}</p>
-            </div>
+            <p className="self-center text-center">{name}</p>
+            <p className="self-center text-center">{hackeroonAmount}</p>
         </div>
     );
 }
