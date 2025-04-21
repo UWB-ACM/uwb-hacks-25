f9bc1fbb4390b7c81b4e0c7f1ed1e3bda0a10b6c
pgobin <pgobin@uw.edu>
2025-04-20 23:49:19 -0700

    changed tracks section to retrieve actual tracks

--- src/components/landing-page/TrackSection.tsx
+++ src/components/landing-page/TrackSection.tsx
@@ -10,16 +10,18 @@ import PanelHeader from "./(AboutSectionComponents)/Panel/Header";
 
 // Mystery Box components / assets
 import MysteryBox from "./(TracksSectionComponents)/MysteryBox";
-import mystery_box_contents_data from "./(TracksSectionComponents)/mysteryBoxContents.json";
+import tracks_data from "./(TracksSectionComponents)/tracks.json";
 
 // Used in Tracks section background
 import QuestionMark from "./(TracksSectionComponents)/QuestionMark";
 
-const TracksSection = () => {
-    // TODO: change from "preHackathonContents" --> "duringHackathonContents" to reveal track names
-    const mysteryBoxContents =
-        mystery_box_contents_data["contents"]["preHackathonContents"];
+// Tab navigation for tracks
+import Tracks from "./(TracksSectionComponents)/TracksTabNavigation/Tracks";
+import { Track } from "@/src/util/dataTypes";
 
+const TracksSection = () => {
+    // tracks contains all relevant information about each of the 6 tracks
+    const tracks: Track[] = tracks_data["tracks"];
     const [showTracks, setShowTracks] = useState(false);
 
     useEffect(() => {
@@ -34,11 +36,6 @@ const TracksSection = () => {
 
     return (
         <Panel id="tracksPanel" className="mt-12 md:mt-16" panelColor="white">
-            <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] top-[10%] left-[8%] rotate-[19deg]" />
-            <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] top-[20%] right-[14%] -rotate-[15deg]" />
-            <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] bottom-[10%] left-[16%] rotate-[28deg]" />
-            <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] bottom-[3%] right-[20%] -rotate-[40deg]" />
-
             <PanelHeader
                 parentPanelId="tracksPanel"
                 as="h2"
@@ -47,38 +44,24 @@ const TracksSection = () => {
             >
                 Tracks
             </PanelHeader>
+
             <PanelContent
                 parentPanelId="tracksPanel"
                 className="relative flex flex-col items-center"
             >
+                <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] top-[10%] left-[8%] rotate-[19deg]" />
+                <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] top-[20%] right-[14%] -rotate-[15deg]" />
+                <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] bottom-[10%] left-[16%] rotate-[28deg]" />
+                <QuestionMark className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[125px] bottom-[3%] right-[20%] -rotate-[40deg]" />
+
+                {/* Pass name of each track as contents of mystery box */}
                 <MysteryBox
-                    contents={mysteryBoxContents}
+                    contents={tracks.map((track) => track["name"])}
                     setShowTracks={setShowTracks}
                 />
-
-                {showTracks && mysteryBoxContents.length == 1 && (
-                    <p
-                        style={{ transform: "translateY(-100vh)" }}
-                        className="tracks font-h1 text-xl md:text-2xl lg:text-3xl md:w-[70%] text-center mt-6"
-                    >
-                        {mysteryBoxContents[0]}
-                    </p>
-                )}
-
-                {showTracks && mysteryBoxContents.length > 1 && (
-                    <div className="w-full mt-6 flex flex-wrap justify-around gap-4">
-                        {mysteryBoxContents.map((content, idx) => (
-                            <p
-                                key={idx}
-                                style={{ transform: "translateY(-100vh)" }}
-                                className="tracks font-h1 text-xl md:text-2xl lg:text-3xl text-center"
-                            >
-                                {content}
-                            </p>
-                        ))}
-                    </div>
-                )}
             </PanelContent>
+
+            {showTracks && <Tracks tracks={tracks} />}
         </Panel>
     );
 };
