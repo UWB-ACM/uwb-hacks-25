19d41d88d80923ca72a40fe59cd37c5163f0bc90
uellenberg <JonahUellenberg@hotmail.com>
2025-04-23 15:24:27 -0700

    Fix prepared statements causing instability

--- src/util/database.ts
+++ src/util/database.ts
@@ -2,6 +2,9 @@ import "server-only";
 
 import postgres from "postgres";
 
-const sql = postgres(process.env.DATABASE_URL as string, {});
+const sql = postgres(process.env.DATABASE_URL as string, {
+    // This doesn't work with Supabase.
+    prepare: false,
+});
 
 export default sql;
