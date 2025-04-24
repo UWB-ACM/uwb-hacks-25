b4d0e41dd8c5c8b9eabc69615cdf6ba1c0d846d4
pgobin <pgobin@uw.edu>
2025-04-24 01:21:49 -0700

    implemented SelectedTrackInformation comp

--- src/components/landing-page/(TracksSectionComponents)/TracksTabNavigation/SelectedTrackInformation.tsx
+++ src/components/landing-page/(TracksSectionComponents)/TracksTabNavigation/SelectedTrackInformation.tsx
@@ -9,13 +9,29 @@ export default function SelectedTrackInformation({
     selectedTrack,
 }: SelectedTrackInformationProps) {
     return (
-        <div className="p-3 md:p-6">
-            <p className="text-center font-h3 font-bold tracking-wider md:text-lg lg:text-xl">
-                {selectedTrack.question}
-            </p>
-            <p>{selectedTrack.description}</p>
-            <p>{selectedTrack.noCodeExample}</p>
-            <p>{selectedTrack.lowCodeExample}</p>
+        <div className="p-3 md:p-6 space-y-4">
+            <div>
+                <p className="font-h2 font-bold tracking-wider text-center text-xl md:text-2xl">
+                    {selectedTrack.question}
+                </p>
+                <p className="mx-auto text-center md:w-3/4 md:text-lg">
+                    {selectedTrack.description}
+                </p>
+            </div>
+            <div className="flex flex-col md:flex-row justify-between gap-y-1 md:gap-x-6 lg:gap-x-4">
+                <div className="md:w-1/2 text-center md:text-left">
+                    <p className="font-bold font-h2 text-lg md:text-xl text-[#49B2F8]">
+                        {selectedTrack.name + ": "} No Code Example
+                    </p>
+                    <p className="md:text-lg">{selectedTrack.noCodeExample}</p>
+                </div>
+                <div className="md:w-1/2 text-center md:text-left">
+                    <p className="font-bold font-h2 text-lg md:text-xl text-[#49B2F8]">
+                        {selectedTrack.name + ": "} Code Example
+                    </p>
+                    <p className="md:text-lg">{selectedTrack.lowCodeExample}</p>
+                </div>
+            </div>
         </div>
     );
 }
