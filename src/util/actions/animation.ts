8dce400e626edcc5ab01fa7026ec080154555180
uellenberg <JonahUellenberg@hotmail.com>
2025-04-27 09:04:55 -0700

    Fix animation stop not saving

--- src/util/actions/animation.ts
+++ src/util/actions/animation.ts
@@ -5,5 +5,13 @@ import { cookies } from "next/headers";
 export async function actionSetTracksAnimation() {
     "use server";
 
-    (await cookies()).set("hackathonTracksShown", "true");
+    // Dates are in milliseconds
+    const animationTimeSeconds = 60 * 60 * 24 * 7;
+    const expiresAt = new Date(Date.now() + animationTimeSeconds * 1000);
+
+    (await cookies()).set({
+        name: "hackathonTracksShown",
+        value: "true",
+        expires: expiresAt,
+    });
 }
