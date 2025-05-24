bbf2dc80816e5727582c2db661577066f44720b3
uellenberg <JonahUellenberg@hotmail.com>
2025-05-24 13:02:50 -0700

    Add content security policy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self'${process.env.NODE_ENV === "development" ? " 'unsafe-eval' 'unsafe-inline'" : ""} https://cdn.strawpoll.com;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    frame-src https://strawpoll.com;
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
     images: {
         formats: ["image/avif", "image/webp"],
@@ -21,6 +35,19 @@ const nextConfig: NextConfig = {
                 hostname: "www.gravatar.com",
             },
         ],
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: cspHeader.replace(/\n/g, ""),
+                    },
+                ],
+            },
+        ];
     },
 };
 
